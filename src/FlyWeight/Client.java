423c8c52cc3dbce2adad734e34c9868bf33b3d0c
luyaqing <879571159@qq.com>
2022-04-10 16:31:33 +0800

    test！！

--- src/FlyWeight/Client.java
+++ src/FlyWeight/Client.java
@@ -11,6 +11,7 @@ public class Client {
     public static void main(String[] args) {
 
         for (int i = 0; i < 4; i++) {
+
             String subject = "科目" + i;
 
             for (int j=0; j<30; j++) {
